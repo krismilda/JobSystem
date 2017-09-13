268583672a17f9f5865d4f32bfed4d9922cad6e3
Milda Jocaite <milda.jocaite@gmail.com>
2017-09-13 19:55:50 +0300

    Add job list

---
+++ JobSystem/App_Data/dbo.Table_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Jobs]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Darbas] NCHAR(30) NULL, 
+    [Sritis] NCHAR(20) NULL, 
+    [Darbo_pradzia] DATETIME NULL, 
+    [Vieta] NCHAR(20) NULL, 
+    [Alga] FLOAT NULL, 
+    [Trukme] INT NULL
+)
---
+++ JobSystem/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSystem.Models
+{
+    public class Job
+    {
+        public int ID { get; set; }
+        public string Darbas { get; set; }
+        public string Sritis { get; set; }
+        public DateTime Darbo_pradzia { get; set; }
+        public string Vieta { get; set; }
+        public float Alga { get; set; }
+        public int Trukme { get; set; }
+    }
+}
