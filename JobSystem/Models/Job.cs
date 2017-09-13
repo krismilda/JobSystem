06c0488155baad967367dae6b42fec1f41a67032
Milda Jocaite <milda.jocaite@gmail.com>
2017-09-13 21:30:26 +0300

    Create Job List

--- JobSystem/Models/Job.cs
+++ JobSystem/Models/Job.cs
@@ -8,6 +8,11 @@ namespace JobSystem.Models
     public class Job
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Work { get; set; }
+        public string Field{ get; set; }
+        public DateTime Start { get; set; }
+        public string Locality { get; set; }
+        public float  salary { get; set; }
+        public string duration { get; set; }
     }
 }
